103bb7771dcbcb66e8e6f96e11d3f03551bdf523
Abrar Burk <burk.abrar@gmail.com>
2020-04-14 14:00:15 +0530

    updates to home

--- src/content/AboutUs/index.js
+++ src/content/AboutUs/index.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, {Fragment, useState} from "react";
+
+import aboutus from '../../data/aboutus.json';
+
+import arun_img from '../../imgs/arun_img.jpg';
+// import gaurav_img from '../../imgs/gaurav.jpg';
+
 const AboutUs = () => {
-  return <div>About Us</div>;
-};
+
+  const imgs = {
+    arun_img,
+    // gaurav_img
+  }
+
+  return(
+    <div className="f-content aboutus bx--grid">
+      <div className='bx--offset-lg-1 bx--col-lg-10 about-us-content-wrapper'>
+        <h1 style={{ fontWeight: '500'}}>About Us</h1>
+        <div className='bx--row about-us-content' >
+          { aboutus.map( (d, i) => (
+            <div key={i} className='bx--col-lg-4 f-person-card'>
+              <div className='f-person-card__img-wrapper' style={{width:'100%'}}>
+                <img src={imgs[d.img]} alt="" />
+              </div>
+              <div className='f-person-card__text-wrapper'>
+              <h3 className='f-person-card__name'>{d.name}</h3>
+              <p className='f-person-card__desc'>{d.desc}</p>
+              </div>
+            </div>
+          )) }
+
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
 export default AboutUs;
--- src/content/Home/index.js
+++ src/content/Home/index.js
@@ -5,22 +5,24 @@ import HomeHowToUse from '../../components/organisms/HomeHowToUse';
 import DidYouKnowIcon from '../../components/icons/DidYouKnowIcon';
 import didYouKnowContent from '../../data/didYouKnowContent.json';
 import DidYouKnow from '../../components/organisms/DidYouKnow';
+import CoverImg from '../../imgs/cover_img.svg';
 
 const Home = (props) =>  {
 
   return (
     <div className='f-content f-home'>
       <div className='f-home__section f-home__cover'>
-        <div className='bx--row' style={{ alignItems: 'center'}}>
-          <div className='f-home__cover-text-wrapper bx--col-lg-6'>
+        <img className='f-home__cover-bg-img' src={CoverImg} alt="_blank"/>
+        <div className='bx--row'>
+          <div className='f-home__cover-text-wrapper bx--offset-lg-6 bx--col-lg-6'>
             <h1 className='f-cover-title'>Himachal Fiscal Data Explorer</h1>
             <h4 className='f-cover-subtitle'>Fiscal Data Explorer is a unique tool where citizens can explore both budgets and spending data of state governments in an easy to comprehend and simple to use manner.</h4>
           </div>
-          <div className='bx--col-lg-6 f-home__cover-img-wrapper'><FHPMapImg style={{width:'100%'}} /></div>
+          {/*<div className='bx--col-lg-6 f-home__cover-img-wrapper'><FHPMapImg style={{width:'100%'}} /></div>*/}
         </div>
       </div>
       <div className='f-home__section f-home__how-to-use'>
-        <h1 className='f-home__section-title'>How to use this Data Explorer?</h1>
+        {/*<h1 className='f-home__section-title'>How to use this Data Explorer?</h1>*/}
         <HomeHowToUse />
       </div>
       {/*<div className='f-home__section f-home__did-you-know'>
@@ -35,9 +37,7 @@ const Home = (props) =>  {
         )) }
         </div>
       </div>*/}
-      <div className='f-home__section f-home__did-you-know'>
-        <DidYouKnow />
-      </div>
+
     </div>
   )
 }
