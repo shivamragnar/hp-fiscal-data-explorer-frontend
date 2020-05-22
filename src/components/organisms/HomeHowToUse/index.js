5a7032b775ceffbb42d61dd3ca033e07867f6da3
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2020-05-22 13:58:32 +0530

    change home page images and delete demand details section

--- src/components/organisms/HomeHowToUse/index.js
+++ src/components/organisms/HomeHowToUse/index.js
@@ -6,6 +6,10 @@ import DidYouKnow from '../DidYouKnow';
 
 import howToUseContent from '../../../data/howToUseContent.json';
 import placeholder from '../../../imgs/placeholder.jpg';
+import budget_receipts from '../../../imgs/home_page_images/details_receipts.png'
+import treasury_exp from '../../../imgs/home_page_images/exp_district_wise_hpfde.png'
+import treasury_rec from '../../../imgs/home_page_images/rec_districtwise_hpfde.png'
+import schemes from '../../../imgs/home_page_images/schemes_hpfde.png'
 
 
 const HomeHowToUse = (props) => {
@@ -16,7 +20,7 @@ const history = useHistory();
 
 const handleClick = linkRoute => history.push(linkRoute);
 
-const imgs = { placeholder }
+const imgs = { placeholder, budget_receipts, treasury_exp, treasury_rec, schemes }
 
 const createImg = (img) => (
   <div className='f-tab-content__img_wrapper bx--col-lg-6'>
