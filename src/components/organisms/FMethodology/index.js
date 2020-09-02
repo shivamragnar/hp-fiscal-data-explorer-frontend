26fe0026766d2fa6098114a5687d83cb0652a011
Shivam Sharma <mail2shivam94@gmail.com>
2020-09-02 19:04:26 +0530

    Added resources in methodology

--- src/components/organisms/FMethodology/index.js
+++ src/components/organisms/FMethodology/index.js
@@ -31,21 +31,117 @@ const Methodology = () => {
 					selected was based on the Manual for Procurements released
 					by the Government of India as well as the Open Contracting
 					Partnership. The data for all the indices was collected from
-					the <a href="https://hptenders.gov.in/nicgep/app" target="_blank">eprocurement portal for Himachal Pradesh</a>. For detailed methods please look{" "}
-					<a href="https://docs.google.com/document/d/1GGx5cYsfuKAziUUHZcjxM3S-MiN6kR_i/edit" target="_blank">here</a>.
+					the{" "}
+					<a
+						href="https://hptenders.gov.in/nicgep/app"
+						target="_blank"
+					>
+						eprocurement portal for Himachal Pradesh
+					</a>
+					. For detailed methods please look{" "}
+					<a
+						href="https://drive.google.com/file/d/1GGx5cYsfuKAziUUHZcjxM3S-MiN6kR_i/view"
+						target="_blank"
+					>
+						here
+					</a>
+					.
 					<br />
 					<br />
+				</p>
+				<p>
+					<strong>List of Resources:</strong>
+					<ul >
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1QH2vAHTUMdb8OXl6k6vQBofW9XsUUgE3/view"
+								target="_blank"
+								className="resource-link"
+							>
+								System Architecture
+							</a>
+						</li>
+						<li>
+							<a
+								href=" https://superset.civicdatalab.in/superset/dashboard/101/"
+								target="_blank"
+								className="resource-link"
+							>
+								Hospital and College Procurement Analysis
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://superset.civicdatalab.in/superset/dashboard/100/"
+								target="_blank"
+								className="resource-link"
+							>
+								Key Performance Indicator Analysis
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://superset.civicdatalab.in/superset/dashboard/102/"
+								target="_blank"
+								className="resource-link"
+							>
+								Health related spending data analysis
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://superset.civicdatalab.in/superset/dashboard/103/"
+								target="_blank"
+								className="resource-link"
+							>
+								Sanitation and water supply related spending
+								analysis
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1kRCNYg0oQgRCRitbXLtVdy2pLDP9fyY5/view?usp=sharing"
+								target="_blank"
+								className="resource-link"
+							>
+								JSON used for the dashboard
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://github.com/CivicDataLab/himachal-pradesh-health-procurement-OCDS"
+								target="_blank"
+								className="resource-link"
+							>
+								OCDS Published Health Procurement Data
+							</a>
+						</li>
+					</ul>
+				</p>
+				<p>
 					<strong>Caveats</strong>
 					<br />
 				</p>
 				<p>
-					The index is limited by the <b>quality of data that is being released</b>, therefore many of the indicators still remain unknown and we are hoping to get more clarity as we work with governments and organizations to better refine this Index. 
-					The mapping to the sdgs while consistent across this data has been done to the best of our abilities and will probably change with time and with more information.
+					The index is limited by the{" "}
+					<b>quality of data that is being released</b>, therefore
+					many of the indicators still remain unknown and we are
+					hoping to get more clarity as we work with governments and
+					organizations to better refine this Index. The mapping to
+					the sdgs while consistent across this data has been done to
+					the best of our abilities and will probably change with time
+					and with more information.
 				</p>
 				<p>
-					For a full discussion of caveats, please see the <a href="https://docs.google.com/document/d/1GGx5cYsfuKAziUUHZcjxM3S-MiN6kR_i/edit" target="_blank">Methods PDF</a>.
+					For a full discussion of caveats, please see the{" "}
+					<a
+						href="https://drive.google.com/file/d/1GGx5cYsfuKAziUUHZcjxM3S-MiN6kR_i/view"
+						target="_blank"
+					>
+						Methods PDF
+					</a>
+					.
 				</p>
-				
 			</div>
 		</div>
 	);
