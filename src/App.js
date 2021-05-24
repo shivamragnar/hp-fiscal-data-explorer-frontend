679695c9ca6fb8497d9b3bab55a4ab7a77baaa3d
Shreya Agrawal <shreyaagrawal0809@gmail.com>
2021-05-24 13:53:42 +0530

    add table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
-import ReactGA from 'react-ga';
-import PropTypes from "prop-types"
+import ReactGA from "react-ga";
+import PropTypes from "prop-types";
 import { Route, Switch, withRouter } from "react-router-dom";
 import axios from "axios";
 import MediaQuery from "react-responsive";
@@ -9,31 +9,35 @@ import { connect } from "react-redux";
 //ACTIONS
 
 //expenditure
-import { getExpSummaryData } from "./actions/exp_summary.js"
+import { getExpSummaryData } from "./actions/exp_summary.js";
 
-import { getExpDemandwiseData } from "./actions/exp_demandwise.js"
-import { getExpDemandwiseFiltersData } from './actions/exp_demandwise_filters';
+import { getExpDemandwiseData } from "./actions/exp_demandwise.js";
+import { getExpDemandwiseFiltersData } from "./actions/exp_demandwise_filters";
 
-import { getExpDistrictwiseData } from "./actions/exp_districtwise.js"
-import { getExpDistrictwiseFiltersData } from './actions/exp_districtwise_filters';
+import { getExpDistrictwiseData } from "./actions/exp_districtwise.js";
+import { getExpDistrictwiseFiltersData } from "./actions/exp_districtwise_filters";
 
 //receipts
-import { getReceiptsData } from "./actions/receipts.js"
-import { getReceiptsFiltersData } from './actions/receipts_filters';
+import { getReceiptsData } from "./actions/receipts.js";
+import { getReceiptsFiltersData } from "./actions/receipts_filters";
 
-import { getReceiptsDistrictwiseData } from "./actions/receipts_districtwise.js"
-import { getReceiptsDistrictwiseFiltersData } from './actions/receipts_districtwise_filters';
+import { getReceiptsDistrictwiseData } from "./actions/receipts_districtwise.js";
+import { getReceiptsDistrictwiseFiltersData } from "./actions/receipts_districtwise_filters";
 
 //schemes
-import { getExpSchemesData } from "./actions/exp_schemes.js"
-import { getExpSchemesFiltersData } from './actions/exp_schemes_filters';
+import { getExpSchemesData } from "./actions/exp_schemes.js";
+import { getExpSchemesFiltersData } from "./actions/exp_schemes_filters";
 
 //COVID
 
-import { getExpCovidData } from "./actions/exp_covid"
+import { getExpCovidData } from "./actions/exp_covid";
 
 //Procurements
-import { getProcurementsData } from "./actions/procurements"
+import {
+  getProcurementsData,
+  getProcurementsDataTenderAPI,
+  getProcurementsDataAwardsAPI,
+} from "./actions/procurements";
 
 //components
 import FHeader1 from "./components/organisms/FHeader1";
@@ -45,7 +49,7 @@ import BudgetHighlights from "./content/BudgetHighlights";
 import ExpSummary from "./content/ExpSummary";
 import ExpDetails from "./content/ExpDetails";
 import ExpDistrictwise from "./content/ExpDistrictwise";
-import ExpCovidTracker from "./content/ExpCovidTracker"
+import ExpCovidTracker from "./content/ExpCovidTracker";
 
 import Receipts from "./content/Receipts";
 import ReceiptsDistrictwise from "./content/ReceiptsDistrictwise";
@@ -56,169 +60,212 @@ import Glossary from "./content/Glossary";
 import AboutUs from "./content/AboutUs";
 import ContactUs from "./content/ContactUs";
 
-import OCPDashboard from "./content/OCPDashboard"
+import OCPDashboard from "./content/OCPDashboard";
 
 //css
 import "./App.scss";
 
 //CONFIG
 const _CONFIG = {
-	initDateRange : ["2020-04-01","2020-10-31"],
-	initActiveFilters : {},
-	initAllFiltersData : [],
-	initRawFilterDataAllHeads : {} //only applies for exp_districtwise, receipts_districtwise & schemes
-}
+  initDateRange: ["2020-04-01", "2020-10-31"],
+  initActiveFilters: {},
+  initAllFiltersData: [],
+  initRawFilterDataAllHeads: {}, //only applies for exp_districtwise, receipts_districtwise & schemes
+};
 
-ReactGA.initialize('UA-89349304-1');
+ReactGA.initialize("UA-89349304-1");
 ReactGA.set({ anonymizeIp: true });
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = ({
+  getExpSummaryData,
 
-		getExpSummaryData,
+  getExpDemandwiseData,
+  getExpDemandwiseFiltersData,
 
-		getExpDemandwiseData,
-	  getExpDemandwiseFiltersData,
+  getExpDistrictwiseData,
+  getExpDistrictwiseFiltersData,
 
-		getExpDistrictwiseData,
-	  getExpDistrictwiseFiltersData,
+  getReceiptsData,
+  getReceiptsFiltersData,
 
-		getReceiptsData,
-	  getReceiptsFiltersData,
+  getReceiptsDistrictwiseData,
+  getReceiptsDistrictwiseFiltersData,
 
-		getReceiptsDistrictwiseData,
-	  getReceiptsDistrictwiseFiltersData,
+  getExpSchemesData,
+  getExpSchemesFiltersData,
 
-		getExpSchemesData,
-		getExpSchemesFiltersData,
+  getExpCovidData,
 
-		getExpCovidData,
+  getProcurementsData,
+  getProcurementsDataTenderAPI,
+  getProcurementsDataAwardsAPI,
 
-		getProcurementsData,
+  location: { pathname },
+}) => {
+  console.log(window.location.origin);
 
-		location : { pathname }
-	}
-) => {
+  const apiCallQueue = [
+    { apiFunc: () => getProcurementsData("2020-2021") },
 
+    { apiFunc: () => getExpSummaryData() },
 
-console.log(window.location.origin);
+    { apiFunc: () => getProcurementsDataTenderAPI("2020-2021") },
 
-const apiCallQueue = [
-	{ apiFunc: () => getProcurementsData("2020-2021") },
+    // { apiFunc: () => getProcurementsDataAwardsAPI() },
 
-	{ apiFunc: () => getExpSummaryData() },
+    {
+      apiFunc: () =>
+        getReceiptsData(_CONFIG.initActiveFilters, _CONFIG.initDateRange),
+    },
+    { apiFunc: () => getReceiptsFiltersData() },
 
-	{ apiFunc: () => getReceiptsData(_CONFIG.initActiveFilters, _CONFIG.initDateRange) },
-	{ apiFunc: () => getReceiptsFiltersData() },
+    {
+      apiFunc: () =>
+        getExpSchemesData(
+          null /*initData*/,
+          _CONFIG.initActiveFilters,
+          _CONFIG.initDateRange
+        ),
+    },
+    {
+      apiFunc: () =>
+        getExpSchemesFiltersData(
+          _CONFIG.initAllFiltersData,
+          _CONFIG.initRawFilterDataAllHeads
+        ),
+    },
 
-	{ apiFunc: () => getExpSchemesData(null /*initData*/, _CONFIG.initActiveFilters, _CONFIG.initDateRange) },
-	{ apiFunc: () => getExpSchemesFiltersData(_CONFIG.initAllFiltersData, _CONFIG.initRawFilterDataAllHeads) },
+    {
+      apiFunc: () =>
+        getReceiptsDistrictwiseData(
+          null /*initData*/,
+          _CONFIG.initActiveFilters,
+          _CONFIG.initDateRange
+        ),
+    },
+    {
+      apiFunc: () =>
+        getReceiptsDistrictwiseFiltersData(
+          _CONFIG.initAllFiltersData,
+          _CONFIG.initRawFilterDataAllHeads
+        ),
+    },
 
-	{ apiFunc: () => getReceiptsDistrictwiseData(null /*initData*/, _CONFIG.initActiveFilters, _CONFIG.initDateRange) },
-	{ apiFunc: () => getReceiptsDistrictwiseFiltersData(_CONFIG.initAllFiltersData, _CONFIG.initRawFilterDataAllHeads) },
+    /*{ apiFunc: () => getExpDemandwiseData(_CONFIG.initActiveFilters, _CONFIG.initDateRange) },*/
 
-	/*{ apiFunc: () => getExpDemandwiseData(_CONFIG.initActiveFilters, _CONFIG.initDateRange) },*/
+    {
+      apiFunc: () =>
+        getExpDistrictwiseData(
+          null /*initData*/,
+          _CONFIG.initActiveFilters,
+          _CONFIG.initDateRange
+        ),
+    },
 
-	{ apiFunc: () => getExpDistrictwiseData(null /*initData*/, _CONFIG.initActiveFilters, _CONFIG.initDateRange) },
+    {
+      apiFunc: () =>
+        getExpDistrictwiseFiltersData(
+          _CONFIG.initAllFiltersData,
+          _CONFIG.initRawFilterDataAllHeads
+        ),
+    },
 
-	{ apiFunc: () => getExpDistrictwiseFiltersData(_CONFIG.initAllFiltersData, _CONFIG.initRawFilterDataAllHeads) },
+    { apiFunc: () => getExpCovidData() },
 
-	{ apiFunc: () => getExpCovidData() },
+    /*{ apiFunc: () => getExpDemandwiseFiltersData() }*/
+  ];
 
-	/*{ apiFunc: () => getExpDemandwiseFiltersData() }*/
-]
+  const fetchApisInQueue = async (idx) => {
+    await apiCallQueue[idx].apiFunc();
+    if (idx + 1 !== apiCallQueue.length) {
+      fetchApisInQueue(idx + 1);
+    }
+  };
 
+  useEffect(() => {
+    // if(pathname.includes("receipts") === true){
+    //  apiCallQueue.unshift(
+    // 	 { apiFunc: () => getReceiptsData(initReceiptsFilters, initReceiptsDateRange) },
+    //  	 { apiFunc: () => getReceiptsFiltersData() }
+    //  )
+    // }
+    fetchApisInQueue(0);
+  }, []);
 
+  console.log(window.location.href);
 
-
-const fetchApisInQueue = async (idx) => {
-		await apiCallQueue[idx].apiFunc();
-		if(idx+1 !== apiCallQueue.length){
-			fetchApisInQueue(idx+1);
-		}
-}
-
- useEffect(() => {
-	 // if(pathname.includes("receipts") === true){
-		//  apiCallQueue.unshift(
-		// 	 { apiFunc: () => getReceiptsData(initReceiptsFilters, initReceiptsDateRange) },
-		//  	 { apiFunc: () => getReceiptsFiltersData() }
-		//  )
-	 // }
-	 fetchApisInQueue(0);
-	 
-
- }, []);
-
- console.log(window.location.href);
-
- return (
-      <div>
-        <FHeader1 />
-          <Switch>
-						<Route exact path="/" component={Home} />
-            <Route exact path="/aboutus" component={AboutUs} />
-            <Route exact path="/contactus" component={ContactUs} />
-						<Route exact path="/glossary" component={Glossary} />
-            <Route exact path="/expenditure/summary" component={ExpSummary} />
-            <Route exact path="/expenditure/details" component={ExpDetails} />
-            <Route exact path="/expenditure/tracker" component={ExpDistrictwise} />
-						<Route exact path="/receipts/districtwise" component={ReceiptsDistrictwise} />
-						<Route exact path="/schemes" component={ExpSchemes} />
-						<Route exact path="/expenditure/covid19" component={ExpCovidTracker} />
-            <Route exact path="/receipts" component={Receipts} />
-            <Route exact path="/procurement/analysis" component={OCPDashboard} />
-          </Switch>
-      </div>
+  return (
+    <div>
+      <FHeader1 />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/aboutus" component={AboutUs} />
+        <Route exact path="/contactus" component={ContactUs} />
+        <Route exact path="/glossary" component={Glossary} />
+        <Route exact path="/expenditure/summary" component={ExpSummary} />
+        <Route exact path="/expenditure/details" component={ExpDetails} />
+        <Route exact path="/expenditure/tracker" component={ExpDistrictwise} />
+        <Route
+          exact
+          path="/receipts/districtwise"
+          component={ReceiptsDistrictwise}
+        />
+        <Route exact path="/schemes" component={ExpSchemes} />
+        <Route exact path="/expenditure/covid19" component={ExpCovidTracker} />
+        <Route exact path="/receipts" component={Receipts} />
+        <Route exact path="/procurement/analysis" component={OCPDashboard} />
+      </Switch>
+    </div>
   );
-}
+};
 
 App.propTypes = {
+  getExpSummaryData: PropTypes.func.isRequired,
 
-	getExpSummaryData : PropTypes.func.isRequired,
+  getExpDemandwiseData: PropTypes.func.isRequired,
+  getExpDemandwiseFiltersData: PropTypes.func.isRequired,
 
-	getExpDemandwiseData : PropTypes.func.isRequired,
-  getExpDemandwiseFiltersData : PropTypes.func.isRequired,
+  getExpDistrictwiseData: PropTypes.func.isRequired,
+  getExpDistrictwiseFiltersData: PropTypes.func.isRequired,
 
-	getExpDistrictwiseData : PropTypes.func.isRequired,
-  getExpDistrictwiseFiltersData : PropTypes.func.isRequired,
+  getReceiptsData: PropTypes.func.isRequired,
+  getReceiptsFiltersData: PropTypes.func.isRequired,
 
-	getReceiptsData : PropTypes.func.isRequired,
-  getReceiptsFiltersData : PropTypes.func.isRequired,
+  getReceiptsDistrictwiseData: PropTypes.func.isRequired,
+  getReceiptsDistrictwiseFiltersData: PropTypes.func.isRequired,
 
-	getReceiptsDistrictwiseData : PropTypes.func.isRequired,
-  getReceiptsDistrictwiseFiltersData : PropTypes.func.isRequired,
+  getExpSchemesData: PropTypes.func.isRequired,
+  getExpSchemesFiltersData: PropTypes.func.isRequired,
 
-	getExpSchemesData : PropTypes.func.isRequired,
-	getExpSchemesFiltersData : PropTypes.func.isRequired,
+  getExpCovidData: PropTypes.func.isRequired,
 
-	getExpCovidData : PropTypes.func.isRequired
+  getProcurementsDataTenderAPI: PropTypes.func.isRequired,
+  getProcurementsDataAwardsAPI: PropTypes.func.isRequired,
+};
 
-}
+const mapStateToProps = (state) => ({});
 
-const mapStateToProps = state => ({
+export default withRouter(
+  connect(mapStateToProps, {
+    getExpSummaryData,
+    getExpDemandwiseData,
+    getExpDemandwiseFiltersData,
+    getExpDistrictwiseData,
+    getExpDistrictwiseFiltersData,
 
-})
+    getReceiptsData,
+    getReceiptsFiltersData,
 
-export default withRouter(connect(
-	mapStateToProps,
-	{
-		getExpSummaryData,
-		getExpDemandwiseData,
-		getExpDemandwiseFiltersData,
-		getExpDistrictwiseData,
-		getExpDistrictwiseFiltersData,
+    getReceiptsDistrictwiseData,
+    getReceiptsDistrictwiseFiltersData,
 
-		getReceiptsData,
-		getReceiptsFiltersData,
+    getExpSchemesData,
+    getExpSchemesFiltersData,
 
-		getReceiptsDistrictwiseData,
-		getReceiptsDistrictwiseFiltersData,
-
-		getExpSchemesData,
-		getExpSchemesFiltersData,
-
-		getExpCovidData,
-		getProcurementsData,
-
-	})(App));
+    getExpCovidData,
+    getProcurementsData,
+    getProcurementsDataTenderAPI,
+    getProcurementsDataAwardsAPI,
+  })(App)
+);
